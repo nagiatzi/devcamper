3edd33b48416d13310b14f07087ecc38b6302454
nagiatzi <nagiatzi@gmail.com>
2019-11-23 19:04:20 +0100

    add: admin routes

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ connectDB();
 const bootcamps = require('./routes/bootcamps');
 const courses  = require('./routes/courses');
 const auth  = require('./routes/auth');
+const users  = require('./routes/users');
+
 
 const app = express();
 
@@ -50,6 +52,8 @@ const PORT = process.env.PORT || 3000;
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
 app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
+
 
 
 app.use(errorHandler);
