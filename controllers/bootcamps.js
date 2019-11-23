cdf5a60d8416035cd8a37379d3bd09a548c11952
nagiatzi <nagiatzi@gmail.com>
2019-11-23 16:49:32 +0100

    add: functionality for user

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -33,14 +33,28 @@ exports.getBootcamp = asyncHandler( async (req, res, next) => {
 // @route POST /api/v1/bootcamps
 // @access Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
-    const bootcamp = await Bootcamp.create(req.body);
+  // Add user to req,body
+  req.body.user = req.user.id;
 
-    res
-    .status(201)
-    .json({ 
-        success: true,
-        data: bootcamp
-    });
+  // Check for published bootcamp
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+  // If the user is not an admin, they can only add one bootcamp
+  if (publishedBootcamp && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `The user with ID ${req.user.id} has already published a bootcamp`,
+        400
+      )
+    );
+  }
+
+  const bootcamp = await Bootcamp.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    data: bootcamp
+  });
 });
 
 // @desc Update a specific bootcamp
--- seeder.js
+++ seeder.js
@@ -3,66 +3,70 @@ const mongoose = require('mongoose');
 const colors = require('colors');
 const dotenv = require('dotenv');
 
-//Load env vars
+// Load env vars
 dotenv.config({ path: './config/config.env' });
 
-//Load Models
+// Load models
 const Bootcamp = require('./models/Bootcamp');
-const Course = require("./models/Course");
+const Course = require('./models/Course');
+const User = require('./models/User');
+const Review = require('./models/Review');
 
-
-
-//connect to db 
- mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
-//Read JSON files
+// Read JSON files
 const bootcamps = JSON.parse(
-    fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
 
-//Read JSON files
 const courses = JSON.parse(
-    fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
 );
 
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
 
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/reviews.json`, 'utf-8')
+);
 
-//import into DB
-
+// Import into DB
 const importData = async () => {
-    try{
-        await Bootcamp.create(bootcamps);
-        await Course.create(courses);
+  try {
+    await Bootcamp.create(bootcamps);
+    await Course.create(courses);
+    await User.create(users);
+    await Review.create(reviews);
+    console.log('Data Imported...'.green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-        console.log('Data Imported ....'.green.inverse);
-        process.exit();
-    } catch (err) {
-        console.log(err);
-    }
-
-}
-
-//Delete data 
-
+// Delete data
 const deleteData = async () => {
-    try{
-        await Bootcamp.deleteMany();
-        await Course.deleteMany();
-        console.log('Data Destroyed ....'.red.inverse);
-        process.exit();
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    await Bootcamp.deleteMany();
+    await Course.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
+    console.log('Data Destroyed...'.red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-}
-
-if(process.argv[2] === '-i'){
-    importData();
+if (process.argv[2] === '-i') {
+  importData();
 } else if (process.argv[2] === '-d') {
-    deleteData();
-}
+  deleteData();
+} 
